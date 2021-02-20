8d9856871053873ca26c41305016c2fa10491eaf
taywin2556 <78680978+taywin2556@users.noreply.github.com>
2021-02-20 16:26:06 +0700

    update

--- app/src/main/java/com/example/ponggame/Bat.java
+++ app/src/main/java/com/example/ponggame/Bat.java
@@ -1,4 +1,53 @@
 package com.example.ponggame;
 
+import android.graphics.RectF;
+
 public class Bat {
+    private RectF mRect;
+    private int mScreenX;
+    private float mXCoord;
+    private float mLength;
+    private float mBatSpeed;
+
+    final int STOPPED = 0;
+    final int LEFT = 1;
+    final int RIGHT = 2;
+    private int mBatMoving = STOPPED;
+
+    Bat(int sx, int sy){
+        mScreenX = sx;
+        mLength = mScreenX / 8;
+        float height = sy / 40;
+        mXCoord = mScreenX / 2;
+        float mYCoord = sy - height;
+        mRect = new RectF(mXCoord, mYCoord,
+                mXCoord + mLength,
+                mYCoord + height);
+        mBatSpeed = mScreenX;
+    }
+
+    RectF getRect(){
+        return mRect;
+    }
+
+    void setMovementState(int state) {
+        mBatMoving = state;
+    }
+
+    void update(long fps){
+        if(mBatMoving == LEFT){
+            mXCoord = mXCoord - mBatSpeed / fps;
+        }
+        if(mBatMoving == RIGHT){
+            mXCoord = mXCoord + mBatSpeed / fps;
+        }
+        if(mXCoord < 0){
+            mXCoord = 0;
+        }
+        else if(mXCoord + mLength > mScreenX){
+            mXCoord = mScreenX - mLength;
+        }
+        mRect.left = mXCoord;
+        mRect.right = mXCoord + mLength;
+    }
 }
--- app/src/main/java/com/example/ponggame/PongGame.java
+++ app/src/main/java/com/example/ponggame/PongGame.java
@@ -7,8 +7,10 @@ import android.graphics.Paint;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.MotionEvent;
+import android.graphics.RectF;
 
-public class PongGame extends SurfaceView implements Runnable {
+public class PongGame extends SurfaceView implements  Runnable{
 
     private int mScreenX;
     private int mScreenY;
@@ -24,13 +26,14 @@ public class PongGame extends SurfaceView implements Runnable {
     private final boolean DEBUGGING = true;
     private long mFPS;
     private final int MILLIS_IN_SECOND = 1000;
-
     private Thread mGameThread = null;
     private volatile boolean mPlaying;
     private boolean mPaused = true;
 
 
+
     public PongGame(Context context, int x, int y) {
+
         super(context);
         mScreenX = x;
         mScreenY = y;
@@ -39,47 +42,53 @@ public class PongGame extends SurfaceView implements Runnable {
         mOurHolder = getHolder();
         mPaint = new Paint();
         mBall = new Ball(mScreenX);
+        mBat = new Bat(mScreenX, mScreenY);
         startNewGame();
+
+
     }
 
-    private void startNewGame(){
+    private void startNewGame() {
         mScore = 0;
         mLives = 3;
         mBall.reset(mScreenX, mScreenY);
+
     }
     private void draw() {
-            if (mOurHolder.getSurface().isValid()) {
-                mCanvas = mOurHolder.lockCanvas(); // Lock the canvas (graphics memory)
-                mCanvas.drawColor(Color.argb(255, 26, 128, 182));
-                mPaint.setColor(Color.argb(255, 255, 255, 255));
-                mPaint.setTextSize(mFontSize);
-                mCanvas.drawText("Score: " + mScore + " Lives: " + mLives,
-
-                        mFontMargin, mFontSize, mPaint);
-                mCanvas.drawRect(mBall.getRect(), mPaint);
-
-                if (DEBUGGING) {
-                    printDebuggingText();
-                }
-                mOurHolder.unlockCanvasAndPost(mCanvas);
+        if (mOurHolder.getSurface().isValid()) {
+            mCanvas = mOurHolder.lockCanvas();
+            mCanvas.drawColor(Color.argb(255, 26, 128, 182));
+            mPaint.setColor(Color.argb(255, 255, 255, 255));
+            mPaint.setTextSize(mFontSize);
+            mCanvas.drawText("Score: " + mScore + " Lives: " + mLives,
+                    mFontMargin, mFontSize, mPaint);
+            mCanvas.drawRect(mBall.getRect(), mPaint);
+            mCanvas.drawRect(mBat.getRect(), mPaint);
+            if (DEBUGGING) {
+                printDebuggingText();
             }
+            mOurHolder.unlockCanvasAndPost(mCanvas);
+
+        }
+
     }
-    private void printDebuggingText(){
+    private void printDebuggingText() {
         int debugSize = mFontSize / 2;
         int debugStart = 150;
         mPaint.setTextSize(debugSize);
-        mCanvas.drawText("FPS: " + mFPS ,  10, debugStart + debugSize, mPaint);
-    }
+        mCanvas.drawText("FPS: " + mFPS,
 
+                10, debugStart + debugSize, mPaint);
+
+    }
     @Override
     public void run() {
-
         while (mPlaying) {
 
             long frameStartTime = System.currentTimeMillis();
             if(!mPaused){
-                update(); // update new positions
-                detectCollisions(); // detect collisions
+                update();
+                detectCollisions();
             }
             draw();
             long timeThisFrame = System.currentTimeMillis() - frameStartTime;
@@ -88,17 +97,7 @@ public class PongGame extends SurfaceView implements Runnable {
             }
         }
     }
-
-    private void detectCollisions() {
-    }
-
-    private void update()
-    {mBall.update(mFPS);
-    }
-
-
     public void pause() {
-
         mPlaying = false;
         try {
             mGameThread.join();
@@ -106,12 +105,75 @@ public class PongGame extends SurfaceView implements Runnable {
             Log.e("Error:", "joining thread");
         }
     }
-        public void resume() {
-            mPlaying = true;
-            mGameThread = new Thread(this);
-            mGameThread.start();
+    public void resume() {
+        mPlaying = true;
+        mGameThread = new Thread(this);
+        mGameThread.start();
+    }
+    public void update(){
+        mBall.update(mFPS);
+        mBat.update(mFPS);
+    }
+    public void detectCollisions() {
+        if(RectF.intersects(mBat.getRect(),mBall.getRect())) {
+            mBall.batBounce(mBat.getRect());
+            mBall.increaseVelocity();
+            float a = mBall.getRect().left;
+            float b = mBall.getRect().right;
+            float c =(b-a)/4;
+            float x1 = a+c;
+            float x2 = a+(c*3);
+            float d = mBall.getRect().left;
+            float e = mBall.getRect().left;
+            float y = (d-e)/2;
+            if(y <= x2 && y >= x1) {
+                mScore= mScore+=100;
+            }
+            else {
+                mScore= mScore+=10;
+            }
 
         }
+
+        if(mBall.getRect().bottom > mScreenY){
+            mBall.reverseYVelocity();
+            mLives--;
+            if(mLives == 0){
+                mPaused = true;
+                startNewGame();
+            }
+        }
+        if(mBall.getRect().top < 0){
+            mBall.reverseYVelocity();
+        }
+        if(mBall.getRect().left < 0){
+            mBall.reverseXVelocity();
+        }
+        if(mBall.getRect().right > mScreenX){
+            mBall.reverseXVelocity();
+            mBall.increaseVelocity();
+            mScore-=5;
+        }
+
+
     }
+    @Override
+    public boolean onTouchEvent(MotionEvent motionEvent) {
+        switch (motionEvent.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN:
+                mPaused = false;
+                if(motionEvent.getX() > mScreenX / 2){
+                    mBat.setMovementState(mBat.RIGHT);
+                }
+                else{
+                    mBat.setMovementState(mBat.LEFT);
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                mBat.setMovementState(mBat.STOPPED);
+                break;
 
-
+        }
+        return true;
+    }
+}
